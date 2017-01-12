de585fb7d6e81818f4fb9675ceca0d567de489cb
Juan <juanjo_23@live.com>
2017-01-12 16:42:05 -0600

    upload files

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,31 +4,18 @@ import { FormsModule }   from '@angular/forms';
 import { RouterModule }  from '@angular/router';
 
 import { AppComponent }        from './app.component';
+import { DashboardComponent }  from './dashboard.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent }     from './heroes.component';
-import { DashboardComponent }  from './dashboard.component';
 import { HeroService } 		     from './hero.service';
 
+import { AppRoutingModule }   from './app-routing.module';
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot([
-      {
-        path: '',
-        redirectTo: '/dashboard',
-        pathMatch: 'full'
-      },
-      {
-        path: 'heroes',
-        component: HeroesComponent
-      },
-      {
-        path: 'dashboard',
-        component: DashboardComponent
-      }
-    ])
+    AppRoutingModule    
   ],
   declarations: [
     AppComponent,
@@ -36,9 +23,7 @@ import { HeroService } 		     from './hero.service';
     HeroDetailComponent,
     HeroesComponent
   ],
-  providers: [
-    HeroService
-  ],
+  providers: [ HeroService ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
--- app/hero.service.ts
+++ app/hero.service.ts
@@ -7,4 +7,9 @@ export class HeroService {
   getHeroes(): Promise<Hero[]> {
     return Promise.resolve(HEROES);
   }
+  getHero(id: number): Promise<Hero> {
+    //return Promise.resolve(HEROES[0])
+    return this.getHeroes()
+      .then(heroes => heroes.find(hero => hero.id === id));
+  }
 }
